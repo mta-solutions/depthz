14f4aed86e7cac4221171717438826f8d2810148
Brian Jones <bojo@bojo.wtf>
2024-06-04 11:05:22 -0800

    add ability to download repo, not just use local

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 
+use self::git::download_git;
 use self::parser::*;
 
 pub mod git;
@@ -8,16 +9,37 @@ pub mod parser;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    pub depthz: String,
+    /// Path to DEPTHZ file
+    #[arg(short, long)]
+    pub path: String,
+
+    /// Optional git repo to download
+    #[arg(short, long)]
+    pub git_url: Option<String>,
+
+    /// Name to give repo if git_url is set
+    #[arg(short, long)]
+    pub name: Option<String>,
 }
 
 fn main() {
     let cli = Cli::parse();
-
     let mut depthz: Vec<Element> = vec![];
 
-    // Read and process starting from initial DEPTHZ
-    parser::parse_json(cli.depthz, &mut depthz).unwrap();
+    // If a git_url and name are provided, download the intitial repo
+    // Otherwise path will point to a local DEPTHZ to start from
+    if let (Some(url), Some(name)) = (cli.git_url, cli.name) {
+        download_git(&Git {
+            url: url.clone(),
+            name: name.clone(),
+            path: Some(cli.path.clone()),
+        });
+        let path = format!("/tmp/{}{}/DEPTHZ", name.clone(), cli.path);
+        parser::parse_json(path, &mut depthz).unwrap();
+    } else {
+        // Read and process starting from initial DEPTHZ
+        parser::parse_json(cli.path, &mut depthz).unwrap();
+    }
 
     println!("{:?}", depthz);
 }
